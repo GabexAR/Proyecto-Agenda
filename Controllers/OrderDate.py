a3f24c5d93cc81babcae59510495bb4c1a827b3f
GabexAR <aronson.gabriel.g@gmail.com>
2023-06-30 15:38:22 -0300

    Update OrderDate.py

--- Controllers/OrderDate.py
+++ Controllers/OrderDate.py
@@ -1,23 +1,24 @@
 import datetime
 
-def order_date(date: str):
-    if len(date) == 10:
-        if date[2] == "-" and date[5] == "-":
-            # El formato es dd-mm-yyyy
-            try:
-                birthday = datetime.datetime.strptime(date, '%d-%m-%Y')
-            except ValueError:
-                print("Formato de fecha incorrecto")
-        elif date[2] == "-" and date[5] == "-":
-            # El formato es mm-dd-yyyy
-            try:
-                birthday = datetime.datetime.strptime(date, '%m-%d-%Y')
-            except ValueError:
-                print("Formato de fecha incorrecto")
-        
+class OrderDate:
+    def order_date(date: str):
+        if len(date) == 10:
+            if date[2] == "-" and date[5] == "-":
+                # El formato es dd-mm-yyyy
+                try:
+                    birthday = datetime.datetime.strptime(date, '%d-%m-%Y')
+                except ValueError:
+                    print("Formato de fecha incorrecto")
+            elif date[2] == "-" and date[5] == "-":
+                # El formato es mm-dd-yyyy
+                try:
+                    birthday = datetime.datetime.strptime(date, '%m-%d-%Y')
+                except ValueError:
+                    print("Formato de fecha incorrecto")
+            
+            else:
+                birthday = datetime.datetime.strptime(date, '%Y-%m-%d')
         else:
-            birthday = datetime.datetime.strptime(date, '%Y-%m-%d')
-    else:
-        print("Formato de fecha incorrecto")
-    
-    return birthday
+            print("Formato de fecha incorrecto")
+        
+        return birthday

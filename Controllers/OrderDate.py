efad1cf35c30257b9a74d75d995033b65a32a8eb
GabexAR <aronson.gabriel.g@gmail.com>
2023-06-30 15:36:24 -0300

    Update OrderDate.py
    Pruebas de ingreso de fecha en distintos formatos para que solo tome yyyy-mm-dd

--- Controllers/OrderDate.py
+++ Controllers/OrderDate.py
@@ -1,12 +1,23 @@
-import datetime 
+import datetime
 
-class OrderDate():
-    def order_date(date: str):
-        try:
-            birthday = datetime.datetime.strptime(date, '%d-%m-%Y')
-        except ValueError:
-            birthday = datetime.datetime.strptime(date, '%m-%d-%Y')
-
-        birthday_order = birthday.strptime(birthday,'%Y-%m-%d')      
-
-        return birthday_order 
+def order_date(date: str):
+    if len(date) == 10:
+        if date[2] == "-" and date[5] == "-":
+            # El formato es dd-mm-yyyy
+            try:
+                birthday = datetime.datetime.strptime(date, '%d-%m-%Y')
+            except ValueError:
+                print("Formato de fecha incorrecto")
+        elif date[2] == "-" and date[5] == "-":
+            # El formato es mm-dd-yyyy
+            try:
+                birthday = datetime.datetime.strptime(date, '%m-%d-%Y')
+            except ValueError:
+                print("Formato de fecha incorrecto")
+        
+        else:
+            birthday = datetime.datetime.strptime(date, '%Y-%m-%d')
+    else:
+        print("Formato de fecha incorrecto")
+    
+    return birthday

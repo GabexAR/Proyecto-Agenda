12b480431a4469176b2e0965c4a4bfa8ec956500
GabexAR <aronson.gabriel.g@gmail.com>
2023-07-03 09:30:07 -0300

    Update OrderDate.py
    Se termino de modificar el order_date. Ahora se puede ingresar cualquier dato en la fecha que si no es un formato adecuado va a seguir pidiendo un formato que se requiere.

--- Controllers/OrderDate.py
+++ Controllers/OrderDate.py
@@ -7,18 +7,28 @@ class OrderDate:
                 # El formato es dd-mm-yyyy
                 try:
                     birthday = datetime.datetime.strptime(date, '%d-%m-%Y')
+                    close = True
                 except ValueError:
                     print("Formato de fecha incorrecto")
+                    close = False
+                    birthday = None
             elif date[2] == "-" and date[5] == "-":
                 # El formato es mm-dd-yyyy
                 try:
                     birthday = datetime.datetime.strptime(date, '%m-%d-%Y')
+                    
                 except ValueError:
                     print("Formato de fecha incorrecto")
-            
+                    close = False
+                    birthday = None
+
+
             else:
                 birthday = datetime.datetime.strptime(date, '%Y-%m-%d')
+                close = True
         else:
             print("Formato de fecha incorrecto")
+            close = False
+            birthday = None
         
-        return birthday
+        return birthday, close
